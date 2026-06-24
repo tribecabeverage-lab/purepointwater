7b594d339a823ad9c29200f4fa2beef9eb86cac8
PurePoint Deploy <deploy@purepointwater.local>
2026-06-24 21:00:10 +0000

    Add Tawk.to live chat widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,6 +83,19 @@ export default function RootLayout({
             gtag('config', 'G-C49RTJ9Y2S');
           `}
         </Script>
+        <Script id="tawk-init" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6a3c44caef51781d49e08b5e/1jrtmpe55';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </head>
       <body className={nunito.className}>
         {children}
